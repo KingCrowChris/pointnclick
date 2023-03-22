bc98e86a55f7d22bc53a62b99c9b959ffb881f07
KingCrowChris <127957199+KingCrowChris@users.noreply.github.com>
2023-03-22 16:37:37 +0100

    my gawd i am tired

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 document.getElementById("maintitle").innerText = "point and click adventure";
+
+const offsetCharacter = 16;
+const gameWindow = document.getElementById("gamewindow");
+const maincharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    maincharacter.style.left = x - offsetCharacter + "px";
+    maincharacter.style.top = y - offsetCharacter + "px";
+
+    console.log(e.target.id);
+    switch(e.target.id){
+        case "brokencrates":
+            console.log("these crates are broken and have been looted, exept for one");
+            break;
+        case "barrel1":
+            console.log("this barrel contains fresh food and drinks, you can smell it for sure");
+            break;
+            default:
+                console.log("nothin here");
+                break;
+    }
+
+}
